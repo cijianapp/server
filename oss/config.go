af25c268ca80f95a732728c3ba84e9905d320ba8
wenyu yang <47671625+wenyuyangpku@users.noreply.github.com>
2020-08-27 19:07:45 +0800

    hide seceret key

--- oss/config.go
+++ oss/config.go
@@ -18,8 +18,8 @@ const OssURL = "http://cijiantest01.oss-us-east-1.aliyuncs.com/"
 
 func ossConfig() *oss.Bucket {
 	endpoint := "http://oss-us-east-1.aliyuncs.com"
-	accessKeyID := "LTAI4FhCjDTkQYUShUdgKZgT"
-	accessKeySecret := "kSu78QuaDX5XzyCXaTZ9nqJrNY5aoM"
+	accessKeyID := "LTAI4FhCjD***TkQYUShUdgKZgT"
+	accessKeySecret := "kSu78QuaDX5Xz***yCXaTZ9nqJrNY5aoM"
 	bucketName := "cijiantest01"
 	client, err := oss.New(endpoint, accessKeyID, accessKeySecret)
 	if err != nil {
